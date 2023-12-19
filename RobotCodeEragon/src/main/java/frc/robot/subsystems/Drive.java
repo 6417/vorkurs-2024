23dcd213537a6cbbfc22c6f73111aa96e0c99c75
Eragon <dracheeragon3@gmail.com>
2023-12-19 21:26:01 +0100

    Change ID for Olaf (TestChassis)

--- RobotCodeEragon/src/main/java/frc/robot/subsystems/Drive.java
+++ RobotCodeEragon/src/main/java/frc/robot/subsystems/Drive.java
@@ -12,27 +12,29 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class Drive extends SubsystemBase {
-  private WPI_TalonFX m_rightMasterMotor = new WPI_TalonFX(Constants.Drive.Motors.rechtsVorne);
+  // private WPI_TalonFX m_rightMasterMotor = new WPI_TalonFX(Constants.Drive.Motors.rechtsVorne);
   private WPI_TalonFX m_leftMasterMotor = new WPI_TalonFX(Constants.Drive.Motors.rechtsHinten);
   private WPI_TalonFX m_rightSklavenMotor = new WPI_TalonFX(Constants.Drive.Motors.linksVorne);
   private WPI_TalonFX m_leftSklavenMotor = new WPI_TalonFX(Constants.Drive.Motors.linksHinten);
-  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMasterMotor, m_rightMasterMotor);
+  //normalerweise auf rightMasterMotor
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leftMasterMotor, m_rightSklavenMotor);
 
   Joystick _joystick = new Joystick(Constants.Drive.joystickPort);
 
   public Drive() {
-    m_rightMasterMotor.configFactoryDefault();
+    // m_rightMasterMotor.configFactoryDefault();
     m_leftMasterMotor.configFactoryDefault();
     m_rightSklavenMotor.configFactoryDefault();
     m_leftSklavenMotor.configFactoryDefault();
 
-    m_rightSklavenMotor.follow(m_rightMasterMotor);
+    // m_rightSklavenMotor.follow(m_rightMasterMotor);
     m_leftSklavenMotor.follow(m_leftMasterMotor);
   }
 
   public void periodic() {
-    m_rightMasterMotor.set(TalonFXControlMode.PercentOutput, 0.1);
-    m_leftMasterMotor.set(TalonFXControlMode.PercentOutput, -0.1);
+    //normalerweise auf rightMasterMotor
+    // m_rightMasterMotor.set(TalonFXControlMode.PercentOutput, 0.1);
+    // m_leftMasterMotor.set(TalonFXControlMode.PercentOutput, -0.1);
   }
 
   public double deadzone(double wert, double threshold) {
